607c95216cccc2ad8818352775f073e413e3200c
Connie <conniey@microsoft.com>
2021-06-03 16:25:53 -0700

    Adding AmqpLink source and target models.

---
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/AmqpLinkTarget.java
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.amqp.models;
+
+import com.azure.core.annotation.Fluent;
+
+/**
+ * Target associated with the link.
+ */
+@Fluent
+public final class AmqpLinkTarget {
+    private AmqpAddress address;
+
+    /**
+     * Sets the local address of the AMQP terminus to connect to.
+     *
+     * @return The local address of the AMQP terminus.
+     */
+    public AmqpAddress getAddress() {
+        return this.address;
+    }
+
+    /**
+     * Sets the address of the AMQP terminus to connect to.
+     *
+     * @param address Address of the AMQP terminus.
+     *
+     * @return The updated {@link AmqpLinkTarget} object.
+     */
+    public AmqpLinkTarget setAddress(AmqpAddress address) {
+        this.address = address;
+        return this;
+    }
+}
