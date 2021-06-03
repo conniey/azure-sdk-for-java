e4c2fb25ea3b66af7e63f18563398a1514efb8ca
Connie <conniey@microsoft.com>
2021-06-03 16:26:59 -0700

    Adding Receiver,Sender, SslVerify modes.

---
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/ReceiverSettleMode.java
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.amqp.models;
+
+/**
+ * The receiver settle mode.
+ *
+ * @see <a href="http://docs.oasis-open.org/amqp/core/v1.0/csprd01/amqp-core-transport-v1.0-csprd01.html#type-receiver-settle-mode">Receiver
+ *     settle mode.</a>
+ */
+public enum ReceiverSettleMode {
+    /**
+     * The receiver will spontaneously settle all incoming transfers.
+     */
+    FIRST(0),
+    /**
+     * The receiver will only settle after sending the disposition to the sender and receiving a disposition indicating
+     * settlement of the delivery from the sender.
+     *
+     * @see DeliveryOutcome
+     */
+    SECOND(1);
+
+    private final int value;
+
+    ReceiverSettleMode(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the receive settle mode.
+     *
+     * @return The value of the receive settle mode.
+     */
+    public int getValue() {
+        return value;
+    }
+}
