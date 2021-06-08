b791376ac9b6a7afe948089f6870524df3647d6f
Connie <conniey@microsoft.com>
2021-06-07 21:25:34 -0700

    Adding CbsTokenCredential.

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/ClientConstants.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/ClientConstants.java
@@ -6,11 +6,14 @@ package com.azure.core.amqp.implementation;
 import java.time.Duration;
 
 public final class ClientConstants {
+    public static final Duration OPERATION_TIMEOUT = Duration.ofSeconds(60);
+
     public static final String NOT_APPLICABLE = "n/a";
     public static final String PLATFORM_INFO = getOSInformation();
     public static final String FRAMEWORK_INFO = getFrameworkInfo();
     // Base sleep wait time.
     public static final Duration SERVER_BUSY_WAIT_TIME = Duration.ofSeconds(4);
+    public static final Duration TOKEN_VALIDITY = Duration.ofMinutes(20);
 
     /**
      * The default maximum allowable size, in bytes, for a batch to be sent.
